1aacfb283cbefe9cccc7713f86eaffff1554eb3e
Alexander Lockshyn <info@lab37.com>
2017-12-04 23:48:27 +0200

    Implemented standardized model validation

--- users/models.go
+++ users/models.go
@@ -6,13 +6,17 @@ import (
 	"time"
 	"errors"
 	"log"
+	"encoding/base64"
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/qor/validations"
 
 	"github.com/ld100/goblet/util/database"
 )
+
+var db *gorm.DB
 
 // TODO: Remove this method, use full-fledged migrations and GORM connection polling instead
 func MigrateUsers() {
@@ -32,23 +36,20 @@ func MigrateUsers() {
 		panic(err)
 		log.Fatal(err)
 	}
+	validations.RegisterCallbacks(db)
 
 	db.DropTable(&User{})
 	db.AutoMigrate(&User{})
 
-	password, err := HashPassword("password")
-	if err != nil {
-		panic(err)
-		//log.Fatal(err)
-	}
-
 	user := User{
-		FirstName:      "John",
-		LastName:       "Doe",
-		Email:          "you@example.com",
-		PasswordDigest: password,
+		FirstName: "John the",
+		LastName:  "Doe",
+		Email:     "you@example.com",
+		Password:  "password",
 	}
 	if db.NewRecord(user) {
+		//errs := db.Create(&user).GetErrors()
+		//fmt.Print(errs)
 		db.Create(&user)
 	}
 
@@ -56,27 +57,32 @@ func MigrateUsers() {
 }
 
 type User struct {
-	ID             uint   `gorm:"primary_key"`
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
-	FirstName      string `gorm:"size:255"`        // Default size for string is 255, reset it with this tag
-	LastName       string `gorm:"size:255"`        // Default size for string is 255, reset it with this tag
-	Email          string `gorm:"not null;unique"` // Set field as not nullable and unique
-	PasswordDigest string
+	ID        uint   `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	FirstName string `gorm:"size:255" valid:"optional"`              // Default size for string is 255, reset it with this tag
+	LastName  string `gorm:"size:255" valid:"optional"`              // Default size for string is 255, reset it with this tag
+	Email     string `gorm:"not null;unique" valid:"required,email"` // Set field as not nullable and unique
+	Password  string `gorm:"size:255"`
 }
 
-// GORM callback
-func (u *User) BeforeSave() (err error) {
-	err = u.Validate()
-	if err != nil {
-		err = errors.New("read only user")
+// GORM callback: Encode password before create
+func (u *User) BeforeCreate() (err error) {
+	// Do not encode already encoded password
+	if !IsBase64(u.Password) {
+		u.Password, err = HashPassword(u.Password)
+		if err != nil {
+			err = errors.New("cannot hash user password")
+			log.Fatal(err)
+		}
 	}
 	return
 }
 
-func (u *User) Validate() (err error) {
-	fmt.Println("Validating User model before save")
-	return
+func (u User) Validate(db *gorm.DB) {
+	if u.FirstName == "Joe" {
+		db.AddError(errors.New("go change your name, it is invalid"))
+	}
 }
 
 func HashPassword(password string) (string, error) {
@@ -86,5 +92,10 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func IsBase64(s string) bool {
+	_, err := base64.StdEncoding.DecodeString(s)
 	return err == nil
 }
