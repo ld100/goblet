de4028c6065e4798c5d848933ce1b9348db77be2
Alexander Lockshyn <info@lab37.com>
2017-12-05 22:55:35 +0200

    moved all database setup code out of main.go

--- main.go
+++ main.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"os"
-
-	"github.com/ld100/goblet/persistence"
-	"github.com/ld100/goblet/persistence/migrate"
+	"github.com/ld100/goblet/persistence/setup"
 	"github.com/ld100/goblet/server/rest"
 )
 
 func main() {
 	// Prepare initial data: create db, run migrations and seeds
-	prepareData()
+	setup.SetupDatabases()
 
 	// Launch CHI-based RESTful HTTP server
 	rest.Serve()
-}
-
-// TODO: Move whole chain of commands to persistence package
-func prepareData() {
-	// Create database if not exist
-	ds := &persistence.DataSource{}
-	// Fetch database credentials from ENVIRONMENT
-	ds.FetchENV()
-	ds.CreateDB(os.Getenv("DB_NAME"))
-
-	// Initiate global ORM var
-	persistence.InitGormDB(ds)
-
-	// Run migrations
-	migrate.Migrate()
-
-	// Run db seed
-	migrate.Seed()
 }
